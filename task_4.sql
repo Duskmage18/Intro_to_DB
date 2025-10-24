c462839b14c627e3c76d7c5ed666bf2f4c9ddd75
Harry Forson <linotopo08@gmail.com>
2025-10-24 20:23:11 +0000

    Fixed table_name = books, bug

--- task_4.sql
+++ task_4.sql
@@ -4,5 +4,5 @@ USE alx_book_store;
 -- Print the full description of the table Books
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'bOOKS'
+WHERE TABLE_NAME = 'books'
 AND TABLE_SCHEMA = 'alx_book_store';
