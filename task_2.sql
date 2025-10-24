fa542f98b84bc77c212de0634039c5dfd7d77149
Harry Forson <linotopo08@gmail.com>
2025-10-24 19:02:28 +0000

    Made changes to file

--- task_2.sql
+++ task_2.sql
@@ -1,18 +1,13 @@
--- task_2.sql
-
--- Create database if not exists
-CREATE DATABASE IF NOT EXISTS alx_book_store;
-
--- Use the created database
+-- Use the alx_book_store database
 USE alx_book_store;
 
--- Creating the Authors table
+-- Create the Authors table
 CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
--- Creating the Books table
+-- Create the Books table
 CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
@@ -22,30 +17,28 @@ CREATE TABLE IF NOT EXISTS Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- Creating the Customers table
+-- Create the Customers table
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) NOT NULL UNIQUE,
-    address TEXT
+    email VARCHAR(215) NOT NULL,
+    address TEXT NOT NULL
 );
 
--- Creating the Orders table
+-- Create the Orders table
 CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL,
-    total_amount DECIMAL(10, 2),
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- Creating the Order_Details table
+-- Create the Order_Details table
 CREATE TABLE IF NOT EXISTS Order_Details (
-    order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity INT NOT NULL,
-    unit_price DECIMAL(10, 2),
+    quantity DOUBLE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
