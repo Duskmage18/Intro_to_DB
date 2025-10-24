21c050169d1ad3b32889328a7de9c489eadd6cbf
Harry Forson <linotopo08@gmail.com>
2025-10-24 20:24:43 +0000

    Fixed bug

--- task_4.sql
+++ task_4.sql
@@ -4,5 +4,5 @@ USE alx_book_store;
 -- Print the full description of the table Books
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'books'
+WHERE TABLE_NAME = 'Books'
 AND TABLE_SCHEMA = 'alx_book_store';
